8c93ce6dcb6fac1e1f6c77215690cec1b8b57dd3
Geoff Sobering <gsobering@verveindustrial.com>
2021-07-14 14:31:23 -0500

    Added '__call__' to FooImpl so an instance can also be a factory.

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class FooImpl(Foo):
     def param(self) -> str:
         return self._param
 
+    def __call__(self, new_foo_param: str) -> Foo:
+        self._param = new_foo_param
+        return self
+
 
 def foo_client(factory: Callable[[str], Foo]) -> str:
     foo = factory("foo_client value-added parameter")
@@ -27,12 +31,17 @@ def foo_factory(foo_param: str) -> Foo:
 
 if __name__ == "__main__":
     res1 = foo_client(lambda foo_param: FooImpl(foo_param))
-    print(f"lambda factory:   '{res1}'")
+    print(f"lambda factory:      '{res1}'")
 
     res2 = foo_client(foo_factory)
-    print(f"def factory:      '{res2}'")
+    print(f"def factory:         '{res2}'")
 
     res3 = foo_client(FooImpl)
-    print(f"Class as factory: '{res3}'")
+    print(f"Class as factory:    '{res3}'")
+
+    foo_impl_instance = FooImpl("foo_impl_instance")
+    print(f"foo_impl_instance.param() = '{foo_impl_instance.param()}'")
+    res4 = foo_client(foo_impl_instance)
+    print(f"Instance as factory: '{res4}'")
 
     print('Done!')
